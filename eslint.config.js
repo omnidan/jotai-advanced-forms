7ebe5037008b145a3c38b6882ec9747949288629
Daniel Bugl <me@omnidan.net>
2025-10-20 13:51:08 +0200

    fix: ignore example in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,7 @@ export default tseslint.config(
       "node_modules",
       "pnpm-lock.yaml",
       "docs",
+      "example",
     ],
   },
   { linterOptions: { reportUnusedDisableDirectives: "error" } },
